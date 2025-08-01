2feae774cf0eb2cc2890f573482b2d6a2738d440
comnk <vhsiao36@gmail.com>
2025-08-01 13:53:06 -0700

    push env variable... not sure if works

--- src/translateText.ts
+++ src/translateText.ts
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-//const GOOGLE_API_KEY = process.env.NEXT_PUBLIC_GOOGLE_API_KEY; // use env var for security
-const API_KEY = "AIzaSyB7jtJuCS_I9F5Z3axMxt5bzTJLhovU1MU"
+const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY; // use env var for security
+
 export async function translateText(text: string, targetLang: string) {
     try {
         const res = await axios.post(
-            `https://translation.googleapis.com/language/translate/v2?key=${API_KEY}`,
+            `https://translation.googleapis.com/language/translate/v2?key=${GOOGLE_API_KEY}`,
             {
             q: text,
             target: targetLang,
